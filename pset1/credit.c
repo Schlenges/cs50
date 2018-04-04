ce6b719920c76934799f8cf26776bf7c7dc2d166
Meike <meike_hankewicz@yahoo.de>
2018-04-04 11:00:40 +0200

    initial commit

---
+++ pset1/credit.c
@@ -0,0 +1,90 @@
+#include <cs50.h>
+#include <stdio.h>
+
+int main(void)
+{
+    // Get User Input
+
+    long long cardnum = get_long_long("Please enter your credit card number: ");
+
+    // Determine how many digits in number for initializing array with the right size
+    long long num = cardnum;
+    int digits = 0;
+
+    while (num > 0)
+    {
+        num /= 10;
+        digits++;
+    }
+
+    int numArray[digits];
+
+    // Get digits and store them in the array
+    int i;
+    num = cardnum;
+
+    for (i = digits - 1; i >= 0; i--)
+    {
+        numArray[i] = num % 10;
+        num /= 10;
+    }
+
+    // Validate via Luhn’s algorithm
+
+    // Multiply every other digit by 2, starting with the number’s second-to-last digit, and then add those products' digits together.
+
+    int sum1 = 0, product = 0;
+
+    for(i = digits - 2; i >= 0; i = i - 2)
+    {
+        product = 2 * numArray[i];
+
+        if (product > 9)
+        {
+            sum1 += (product / 10) + (product % 10);
+        }
+        else
+        {
+            sum1 += product;
+        }
+    }
+
+    // Add the sum to the sum of the digits that weren’t multiplied by 2.
+
+    int sum2 = 0;
+
+    for (i = digits - 1; i >= 0; i = i - 2)
+    {
+        sum2 += numArray[i];
+    }
+
+    int sum = sum1 + sum2;
+
+    // Check if the total’s last digit is 0
+
+    if (sum % 10 == 0)
+    {
+        // Check the credit institute
+
+        if (digits == 15 && numArray[0] == 3 && (numArray[1] == 4 || numArray[1] == 7))
+        {
+            printf("AMEX\n");
+        }
+        else if (digits == 16 && numArray[0] == 5 && (numArray[1] > 0 && numArray[1] < 6))
+        {
+            printf("MASTERCARD\n");
+        }
+        else if ((digits == 13 || digits == 16) && numArray[0] == 4)
+        {
+            printf("VISA\n");
+        }
+        else
+        {
+            printf("INVALID\n");
+        }
+    }
+    else
+    {
+        printf("INVALID\n");
+    }
+}
